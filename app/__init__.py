38bfcb64e4d8c8fa4a2797b74f2b79c22d1b6266
kennymaina <victormurigi@outlook.com>
2019-02-07 10:09:24 +0300

    initial submission

--- app/__init__.py
+++ app/__init__.py
@@ -2,25 +2,25 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 
-# def create_app(config_name):
-#     app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
 
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+    # # Creating the app configurations
+    # app.config.from_object(config_options[config_name])
 
-    # Initializing flask extensions
-    bootstrap.init_app(app)
+    # # Initializing flask extensions
+    # bootstrap.init_app(app)
 
-    # Registering the blueprint
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-
-    # setting config
-    from .request import configure_request
-    configure_request(app)
+    # # Registering the blueprint
+    # from .main import main as main_blueprint
+    # app.register_blueprint(main_blueprint)
 
 
-    return app
+    # # setting config
+    # from .request import configure_request
+    # configure_request(app)
+
+
+    # return app
