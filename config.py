f2cea044665632cc7687289506abd4cc91e9dfcc
kennymaina <victormurigi@outlook.com>
2019-02-07 10:29:44 +0300

    config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-# import os
+import os
 
-class Config:
-    '''
-    General configuration parent class
-    '''
-    NEWSHIGHLIGHT_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
-    # NEWSHIGHLIGHT_API_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    NEWSHIGHLIGHT_API_KEY = os.environ.get('NEWSHIGHLIGHT_API_KEY')
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
+# class Config:
+#     '''
+#     General configuration parent class
+#     '''
+#     NEWSHIGHLIGHT_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+#     # NEWSHIGHLIGHT_API_ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+#     NEWSHIGHLIGHT_API_KEY = os.environ.get('NEWSHIGHLIGHT_API_KEY')
+#     # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
