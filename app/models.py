a79a861aba7763d80eb456b279ea27c1c458922c
kennymaina <victormurigi@outlook.com>
2019-02-07 10:13:34 +0300

    app/models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
-class Source:
-    """
-    Source class to define Source Objects
-    """
-    def __init__(self,id,name,description):
-        self.id = id
-        self.name =name
-        self.description =description
+# class Source:
+#     """
+#     Source class to define Source Objects
+#     """
+#     def __init__(self,id,name,description):
+#         self.id = id
+#         self.name =name
+#         self.description =description
 
 
 class Article:
