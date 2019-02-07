65c89ae59f9614ed80e8ecdada85016a6289c8e3
kennymaina <victormurigi@outlook.com>
2019-02-07 10:15:27 +0300

    changes in models.py

--- app/models.py
+++ app/models.py
@@ -1,26 +1,26 @@
-# class Source:
-#     """
-#     Source class to define Source Objects
-#     """
-#     def __init__(self,id,name,description):
-#         self.id = id
-#         self.name =name
-#         self.description =description
-
-
-class Article:
+class Source:
     """
-    Source class to define Article Objects
+    Source class to define Source Objects
     """
-    def __init__(self,id,name,author,title,description,publishedAt,url,urlToImage):
-
-        self.id =id
-        self.name = name
-        self.author=author
-        self.title=title
+    def __init__(self,id,name,description):
+        self.id = id
+        self.name =name
         self.description =description
-        self.publishedAt=publishedAt
-        self.url =url
-        self.urlToImage=urlToImage
+
+
+# class Article:
+#     """
+#     Source class to define Article Objects
+#     """
+#     def __init__(self,id,name,author,title,description,publishedAt,url,urlToImage):
+
+#         self.id =id
+#         self.name = name
+#         self.author=author
+#         self.title=title
+#         self.description =description
+#         self.publishedAt=publishedAt
+#         self.url =url
+#         self.urlToImage=urlToImage
     
        
