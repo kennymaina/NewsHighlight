344fc2e82c3d51cf2202db1c50d94e35adcac378
kennymaina <victormurigi@outlook.com>
2019-02-07 10:32:15 +0300

    config changes

--- config.py
+++ config.py
@@ -10,23 +10,23 @@ class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-# class ProdConfig(Config):
-#     '''
-#     Production  configuration child class
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     pass
-
-
-class DevConfig(Config):
+class ProdConfig(Config):
     '''
-    Development  configuration child class
+    Production  configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    pass
 
-    DEBUG = True
+
+# class DevConfig(Config):
+#     '''
+#     Development  configuration child class
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+
+#     DEBUG = True
 
 config_options = {
 'development':DevConfig,
